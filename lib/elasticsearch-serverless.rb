31d9fdc57fa15134e8d0c637df734e61efd4fcad
Fernando Briano <fernando@picandocodigo.net>
2023-04-13 10:23:16 +0100

    Implements integration with Transport in the client code

--- lib/elasticsearch-serverless.rb
+++ lib/elasticsearch-serverless.rb
@@ -3,8 +3,33 @@ require 'elasticsearch-serverless/version'
 
 module ElasticsearchServerless
   class Client
-    def initialize()
-      puts "You initialized a client!"
+    # Initializes an Elasticsearch Serverless Client
+    #
+    # @param :api_key [String] Base64 String, format used to authenticate with Elasticsearch
+    # @param :url [String] Elasticsearch endpoint
+    # @param :arguments [Hash] Other optional arguments.
+    # TODO: See which arguments we'll allow for Serverless.
+    #
+    def initialize(api_key:, url:, arguments: {})
+      # TODO: Change this if we allow more arguments
+      arguments.merge!(
+        transport_options: { headers: { 'Authorization' => "ApiKey #{api_key}" } },
+        host: url
+      )
+      @transport = Elastic::Transport::Client.new(arguments)
+    end
+
+    # Metaprogramming for integration with Transport
+    def method_missing(name, *args, &block)
+      if methods.include?(name)
+        super
+      else
+        @transport.send(name, *args, &block)
+      end
+    end
+
+    def respond_to_missing?(method_name, *args)
+      @transport.respond_to?(method_name) || super
     end
   end
 end
