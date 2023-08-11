82ed6b0cb6f6d6037c370efba1f8ac42f9874ece
Fernando Briano <fernando@picandocodigo.net>
2023-08-11 14:50:17 +0100

    [API] Adds exists API

---
+++ lib/elasticsearch-serverless/api/exists.rb
@@ -0,0 +1,67 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Auto generated from commit f284cc16f4d4b4289bc679aa1529bb504190fe80
+# @see https://github.com/elastic/elasticsearch-specification
+#
+module ElasticsearchServerless
+  module API
+    module Actions
+      # Returns information about whether a document exists in an index.
+      #
+      # @option arguments [String] :id The document ID (*Required*)
+      # @option arguments [String] :index The name of the index (*Required*)
+      # @option arguments [String] :preference Specify the node or shard the operation should be performed on (default: random)
+      # @option arguments [Boolean] :realtime Specify whether to perform the operation in realtime or search mode
+      # @option arguments [Boolean] :refresh Refresh the shard containing the document before performing the operation
+      # @option arguments [String] :routing Specific routing value
+      # @option arguments [Boolean, String, Array<String>] :_source True or false to return the _source field or not, or a list of fields to return
+      # @option arguments [String, Array<String>] :_source_excludes A list of fields to exclude from the returned _source field
+      # @option arguments [String, Array<String>] :_source_includes A list of fields to extract and return from the _source field
+      # @option arguments [String, Array<String>] :stored_fields A comma-separated list of stored fields to return in the response
+      # @option arguments [Integer] :version Explicit version number for concurrency control
+      # @option arguments [String] :version_type Specific version type
+      # @option arguments [Hash] :headers Custom HTTP headers
+      #
+      # @see https://www.elastic.co/guide/en/elasticsearch/reference/master/docs-get.html
+      #
+      def exists(arguments = {})
+        raise ArgumentError, "Required argument 'index' missing" unless arguments[:index]
+        raise ArgumentError, "Required argument 'id' missing" unless arguments[:id]
+
+        arguments = arguments.clone
+        headers = arguments.delete(:headers) || {}
+
+        body = nil
+
+        _id = arguments.delete(:id)
+
+        _index = arguments.delete(:index)
+
+        method = ElasticsearchServerless::API::HTTP_HEAD
+        path   = "#{Utils.listify(_index)}/_doc/#{Utils.listify(_id)}"
+        params = Utils.process_params(arguments)
+
+        Utils.rescue_from_not_found do
+          perform_request(method, path, params, body, headers).status == 200 ? true : false
+        end
+      end
+
+      alias_method :exists?, :exists
+    end
+  end
+end
