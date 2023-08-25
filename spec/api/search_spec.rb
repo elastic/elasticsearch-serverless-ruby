7f2a48659022ac35b8a69b746ead4e6e62edc16f
Fernando Briano <fernando@picandocodigo.net>
2023-08-25 09:00:04 +0100

    [API] Updates search test

--- spec/api/search_spec.rb
+++ spec/api/search_spec.rb
@@ -20,23 +20,30 @@ require 'spec_helper'
 describe 'API:search' do
   let(:client) do
     ElasticsearchServerless::Client.new(
-      api_key: 'api_key',
+      api_key: 'my_api_key',
       url: 'https://my-deployment.elastic.co'
     )
+  end
+  let(:index) { 'search_test' }
+
+  after do
+    VCR.use_cassette("#{index}_delete_index") do
+      client.indices.delete(index: index, ignore: 404)
+    end
   end
 
   it 'performs the request' do
     VCR.use_cassette('search') do
       body = [
-        { index: { _index: 'books', _id: '42' } },
+        { index: { _index: index, _id: '42' } },
         { name: "The Hitchhiker's Guide to the Galaxy", author: 'Douglas Adams', release_date: '1979-10-12', page_count: 180},
-        { index: { _index: 'books', _id: '43' } },
+        { index: { _index: index, _id: '43' } },
         { name: 'Snow Crash', author: 'Neal Stephenson', release_date: '1992-06-01', page_count: 470 },
-        { index: { _index: 'books', _id: '44' } },
+        { index: { _index: index, _id: '44' } },
         { name: 'Starship Troopers', author: 'Robert A. Heinlein', release_date: '1959-12-01', page_count: 335}
       ]
-      client.bulk(body: body)
-      response = client.search(index: 'books', q: 'snow')
+      client.bulk(body: body, refresh: true)
+      response = client.search(index: index, q: 'snow')
       expect(response.status).to eq 200
       expect(response['hits']['hits'].first['_source']).to eq ({
                                                                  'name' => 'Snow Crash',
