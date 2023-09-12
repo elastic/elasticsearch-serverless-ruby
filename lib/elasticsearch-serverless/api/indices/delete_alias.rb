b2d19de0f82bbce1991e6531b5fb87fba7e99d0a
Fernando Briano <fernando@picandocodigo.net>
2023-09-12 15:14:03 +0100

    [API] Adds alias APIs: - indices.delete_alias.rb - indices.exists_alias.rb - indices.get_alias.rb - indices.put_alias.rb - indices.update_aliases.rb

---
+++ lib/elasticsearch-serverless/api/indices/delete_alias.rb
@@ -0,0 +1,61 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Auto generated from commit f284cc16f4d4b4289bc679aa1529bb504190fe80
+# @see https://github.com/elastic/elasticsearch-specification
+#
+module ElasticsearchServerless
+  module API
+    module Indices
+      module Actions
+        # Deletes an alias.
+        #
+        # @option arguments [String, Array] :index Comma-separated list of data streams or indices used to limit the request. Supports wildcards (+*+). (*Required*)
+        # @option arguments [String, Array<String>] :name Comma-separated list of aliases to remove. Supports wildcards (+*+). To remove all aliases, use +*+ or +_all+. (*Required*)
+        # @option arguments [Time] :master_timeout Period to wait for a connection to the master node. If no response is received before the timeout expires, the request fails and returns an error. Server default: 30s.
+        # @option arguments [Time] :timeout Period to wait for a response. If no response is received before the timeout expires, the request fails and returns an error. Server default: 30s.
+        # @option arguments [Hash] :headers Custom HTTP headers
+        #
+        # @see https://www.elastic.co/guide/en/elasticsearch/reference/master/indices-aliases.html
+        #
+        def delete_alias(arguments = {})
+          raise ArgumentError, "Required argument 'index' missing" unless arguments[:index]
+          raise ArgumentError, "Required argument 'name' missing" unless arguments[:name]
+
+          arguments = arguments.clone
+          headers = arguments.delete(:headers) || {}
+
+          body = nil
+
+          _index = arguments.delete(:index)
+
+          _name = arguments.delete(:name)
+
+          method = ElasticsearchServerless::API::HTTP_DELETE
+          path   = if _index && _name
+                     "#{Utils.listify(_index)}/_aliases/#{Utils.listify(_name)}"
+                   end
+          params = Utils.process_params(arguments)
+
+          ElasticsearchServerless::API::Response.new(
+            perform_request(method, path, params, body, headers)
+          )
+        end
+      end
+    end
+  end
+end
