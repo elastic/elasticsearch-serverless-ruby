720abbe14cb5696fcb4ec1d1df588beb18954896
Fernando Briano <fernando@picandocodigo.net>
2023-09-13 15:49:45 +0100

    [API] Adds cluster.exists_component_template

---
+++ lib/elasticsearch-serverless/api/cluster/exists_component_template.rb
@@ -0,0 +1,57 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Auto generated from commit f284cc16f4d4b4289bc679aa1529bb504190fe80
+# @see https://github.com/elastic/elasticsearch-specification
+#
+module ElasticsearchServerless
+  module API
+    module Cluster
+      module Actions
+        # Returns information about whether a particular component template exist
+        #
+        # @option arguments [String, Array<String>] :name Comma-separated list of component template names used to limit the request. Wildcard (*) expressions are supported. (*Required*)
+        # @option arguments [Time] :master_timeout Period to wait for a connection to the master node. If no response is received before the timeout expires, the request fails and returns an error. Server default: 30s.
+        # @option arguments [Boolean] :local If true, the request retrieves information from the local node only. Defaults to false, which means information is retrieved from the master node.
+        # @option arguments [Hash] :headers Custom HTTP headers
+        #
+        # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/indices-component-template.html
+        #
+        def exists_component_template(arguments = {})
+          raise ArgumentError, "Required argument 'name' missing" unless arguments[:name]
+
+          arguments = arguments.clone
+          headers = arguments.delete(:headers) || {}
+
+          body = nil
+
+          _name = arguments.delete(:name)
+
+          method = ElasticsearchServerless::API::HTTP_HEAD
+          path   = "_component_template/#{Utils.listify(_name)}"
+          params = Utils.process_params(arguments)
+
+          ElasticsearchServerless::API::Response.new(
+            perform_request(method, path, params, body, headers)
+          )
+        end
+
+        alias_method :exists_component_template?, :exists_component_template
+      end
+    end
+  end
+end
