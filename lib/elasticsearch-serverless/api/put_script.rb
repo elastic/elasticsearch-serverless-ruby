98ece93566a19e1837136a345db2f60beb275e1e
Fernando Briano <fernando@picandocodigo.net>
2023-10-02 14:00:34 +0100

    [API] Adds delete, get and put script APIs

---
+++ lib/elasticsearch-serverless/api/put_script.rb
@@ -0,0 +1,62 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Auto generated from commit f284cc16f4d4b4289bc679aa1529bb504190fe80
+# @see https://github.com/elastic/elasticsearch-specification
+#
+module ElasticsearchServerless
+  module API
+    module Actions
+      # Creates or updates a script.
+      #
+      # @option arguments [String] :id Identifier for the stored script or search template. Must be unique within the cluster. (*Required*)
+      # @option arguments [String] :context Context in which the script or search template should run. To prevent errors, the API immediately compiles the script or template in this context.
+      # @option arguments [Time] :master_timeout Period to wait for a connection to the master node. If no response is received before the timeout expires, the request fails and returns an error. Server default: 30s.
+      # @option arguments [Time] :timeout Period to wait for a response. If no response is received before the timeout expires, the request fails and returns an error. Server default: 30s.
+      # @option arguments [Hash] :headers Custom HTTP headers
+      # @option arguments [Hash] :body request body
+      #
+      # @see https://www.elastic.co/guide/en/elasticsearch/reference/master/modules-scripting.html
+      #
+      def put_script(arguments = {})
+        raise ArgumentError, "Required argument 'body' missing" unless arguments[:body]
+        raise ArgumentError, "Required argument 'id' missing" unless arguments[:id]
+
+        arguments = arguments.clone
+        headers = arguments.delete(:headers) || {}
+
+        body = arguments.delete(:body)
+
+        _id = arguments.delete(:id)
+
+        _context = arguments.delete(:context)
+
+        method = ElasticsearchServerless::API::HTTP_PUT
+        path   = if _id && _context
+                   "_scripts/#{Utils.listify(_id)}/#{Utils.listify(_context)}"
+                 else
+                   "_scripts/#{Utils.listify(_id)}"
+                 end
+        params = Utils.process_params(arguments)
+
+        ElasticsearchServerless::API::Response.new(
+          perform_request(method, path, params, body, headers)
+        )
+      end
+    end
+  end
+end
