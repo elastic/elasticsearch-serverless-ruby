4b7be5800f52fd930e0b3df24b122476cd314c8c
Fernando Briano <fernando@picandocodigo.net>
2023-10-04 09:18:34 +0100

    [API] Adds terms_enum API

---
+++ lib/elasticsearch-serverless/api/terms_enum.rb
@@ -0,0 +1,57 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Auto generated from commit f284cc16f4d4b4289bc679aa1529bb504190fe80
+# @see https://github.com/elastic/elasticsearch-specification
+#
+module ElasticsearchServerless
+  module API
+    module Actions
+      # The terms enum API  can be used to discover terms in the index that begin with the provided string. It is designed for low-latency look-ups used in auto-complete scenarios.
+      #
+      # @option arguments [String] :index Comma-separated list of data streams, indices, and index aliases to search. Wildcard (*) expressions are supported. (*Required*)
+      # @option arguments [Hash] :headers Custom HTTP headers
+      # @option arguments [Hash] :body request body
+      #
+      # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/search-terms-enum.html
+      #
+      def terms_enum(arguments = {})
+        raise ArgumentError, "Required argument 'index' missing" unless arguments[:index]
+
+        arguments = arguments.clone
+        headers = arguments.delete(:headers) || {}
+
+        body = arguments.delete(:body)
+
+        _index = arguments.delete(:index)
+
+        method = if body
+                   ElasticsearchServerless::API::HTTP_POST
+                 else
+                   ElasticsearchServerless::API::HTTP_GET
+                 end
+
+        path   = "#{Utils.listify(_index)}/_terms_enum"
+        params = {}
+
+        ElasticsearchServerless::API::Response.new(
+          perform_request(method, path, params, body, headers)
+        )
+      end
+    end
+  end
+end
