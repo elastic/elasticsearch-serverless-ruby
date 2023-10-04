dcfc2f5a7922d28ef640c73ba29ef62983763da6
Fernando Briano <fernando@picandocodigo.net>
2023-10-04 15:53:05 +0100

    [API] Adds enrich endpoints

---
+++ lib/elasticsearch-serverless/api/enrich/get_policy.rb
@@ -0,0 +1,55 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Auto generated from commit f284cc16f4d4b4289bc679aa1529bb504190fe80
+# @see https://github.com/elastic/elasticsearch-specification
+#
+module ElasticsearchServerless
+  module API
+    module Enrich
+      module Actions
+        # Gets information about an enrich policy.
+        #
+        # @option arguments [String, Array<String>] :name Comma-separated list of enrich policy names used to limit the request. To return information for all enrich policies, omit this parameter.
+        # @option arguments [Hash] :headers Custom HTTP headers
+        #
+        # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/get-enrich-policy-api.html
+        #
+        def get_policy(arguments = {})
+          arguments = arguments.clone
+          headers = arguments.delete(:headers) || {}
+
+          body = nil
+
+          _name = arguments.delete(:name)
+
+          method = ElasticsearchServerless::API::HTTP_GET
+          path   = if _name
+                     "_enrich/policy/#{Utils.listify(_name)}"
+                   else
+                     "_enrich/policy"
+                   end
+          params = {}
+
+          ElasticsearchServerless::API::Response.new(
+            perform_request(method, path, params, body, headers)
+          )
+        end
+      end
+    end
+  end
+end
