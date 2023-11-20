8d0cea0cf25c81b1c9610c2805af64cd4a056095
Fernando Briano <fernando@picandocodigo.net>
2023-11-20 15:39:06 +0000

    [API] Updates delete search application docs url

--- lib/elasticsearch-serverless/api/search_application/delete.rb
+++ lib/elasticsearch-serverless/api/search_application/delete.rb
@@ -31,7 +31,7 @@ module ElasticsearchServerless
         # @option arguments [String] :name The name of the search application to delete (*Required*)
         # @option arguments [Hash] :headers Custom HTTP headers
         #
-        # @see https://www.elastic.co/guide/en/elasticsearch/reference/master/put-search-application.html
+        # @see https://www.elastic.co/guide/en/elasticsearch/reference/master/delete-search-application.html
         #
         def delete(arguments = {})
           raise ArgumentError, "Required argument 'name' missing" unless arguments[:name]
