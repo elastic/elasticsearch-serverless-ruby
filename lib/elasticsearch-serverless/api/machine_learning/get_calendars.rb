f9d5e71461d51b01f12d60e44bb8ee4cbc46b186
Fernando Briano <fernando@picandocodigo.net>
2023-12-11 13:35:10 +0000

    [API] Adds Machine Learning get, put and delete calendar APIs

---
+++ lib/elasticsearch-serverless/api/machine_learning/get_calendars.rb
@@ -0,0 +1,63 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Auto generated from commit f284cc16f4d4b4289bc679aa1529bb504190fe80
+# @see https://github.com/elastic/elasticsearch-specification
+#
+module ElasticsearchServerless
+  module API
+    module MachineLearning
+      module Actions
+        # Retrieves configuration information for calendars.
+        #
+        # @option arguments [String] :calendar_id A string that uniquely identifies a calendar. You can get information for multiple calendars by using a comma-separated list of ids or a wildcard expression. You can get information for all calendars by using +_all+ or +*+ or by omitting the calendar identifier.
+        # @option arguments [Integer] :from Skips the specified number of calendars. This parameter is supported only when you omit the calendar identifier. Server default: 0.
+        # @option arguments [Integer] :size Specifies the maximum number of calendars to obtain. This parameter is supported only when you omit the calendar identifier. Server default: 10000.
+        # @option arguments [Hash] :headers Custom HTTP headers
+        # @option arguments [Hash] :body request body
+        #
+        # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/ml-get-calendar.html
+        #
+        def get_calendars(arguments = {})
+          arguments = arguments.clone
+          headers = arguments.delete(:headers) || {}
+
+          body = arguments.delete(:body)
+
+          _calendar_id = arguments.delete(:calendar_id)
+
+          method = if body
+                     ElasticsearchServerless::API::HTTP_POST
+                   else
+                     ElasticsearchServerless::API::HTTP_GET
+                   end
+
+          path   = if _calendar_id
+                     "_ml/calendars/#{Utils.listify(_calendar_id)}"
+                   else
+                     "_ml/calendars"
+                   end
+          params = Utils.process_params(arguments)
+
+          ElasticsearchServerless::API::Response.new(
+            perform_request(method, path, params, body, headers)
+          )
+        end
+      end
+    end
+  end
+end
