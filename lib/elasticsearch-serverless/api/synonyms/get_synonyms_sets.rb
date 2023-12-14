6f7a6c9e349e9471e73c016a82008109d6bb7351
Fernando Briano <fernando@picandocodigo.net>
2023-12-14 11:01:30 +0000

    [API] Promotes synonyms APIs from experimental to stable

--- lib/elasticsearch-serverless/api/synonyms/get_synonyms_sets.rb
+++ lib/elasticsearch-serverless/api/synonyms/get_synonyms_sets.rb
@@ -23,10 +23,6 @@ module ElasticsearchServerless
     module Synonyms
       module Actions
         # Retrieves a summary of all defined synonym sets
-        # This functionality is Experimental and may be changed or removed
-        # completely in a future release. Elastic will take a best effort approach
-        # to fix any issues, but experimental features are not subject to the
-        # support SLA of official GA features.
         #
         # @option arguments [Integer] :from Starting offset Server default: 0.
         # @option arguments [Integer] :size specifies a max number of results to get Server default: 10.
