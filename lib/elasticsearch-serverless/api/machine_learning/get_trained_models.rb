357f1d3791a3c90d5ee875e0a55d308f2224292a
Fernando Briano <fernando@picandocodigo.net>
2023-12-18 15:37:43 +0000

    [API] Adds ml trained model delete, get and put

---
+++ lib/elasticsearch-serverless/api/machine_learning/get_trained_models.rb
@@ -0,0 +1,62 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Auto generated from commit f284cc16f4d4b4289bc679aa1529bb504190fe80
+# @see https://github.com/elastic/elasticsearch-specification
+#
+module ElasticsearchServerless
+  module API
+    module MachineLearning
+      module Actions
+        # Retrieves configuration information for a trained inference model.
+        #
+        # @option arguments [String] :model_id The unique identifier of the trained model.
+        # @option arguments [Boolean] :allow_no_match Specifies what to do when the request:  - Contains wildcard expressions and there are no models that match. - Contains the _all string or no identifiers and there are no matches. - Contains wildcard expressions and there are only partial matches.  If true, it returns an empty array when there are no matches and the subset of results when there are partial matches. Server default: true.
+        # @option arguments [Boolean] :decompress_definition Specifies whether the included model definition should be returned as a JSON map (true) or in a custom compressed format (false). Server default: true.
+        # @option arguments [Boolean] :exclude_generated Indicates if certain fields should be removed from the configuration on retrieval. This allows the configuration to be in an acceptable format to be retrieved and then added to another cluster.
+        # @option arguments [Integer] :from Skips the specified number of models. Server default: 0.
+        # @option arguments [String] :include A comma delimited string of optional fields to include in the response body.
+        # @option arguments [Integer] :size Specifies the maximum number of models to obtain. Server default: 100.
+        # @option arguments [String] :tags A comma delimited string of tags. A trained model can have many tags, or none. When supplied, only trained models that contain all the supplied tags are returned.
+        # @option arguments [Hash] :headers Custom HTTP headers
+        #
+        # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/get-trained-models.html
+        #
+        def get_trained_models(arguments = {})
+          arguments = arguments.clone
+          headers = arguments.delete(:headers) || {}
+
+          body = nil
+
+          _model_id = arguments.delete(:model_id)
+
+          method = ElasticsearchServerless::API::HTTP_GET
+          path   = if _model_id
+                     "_ml/trained_models/#{Utils.listify(_model_id)}"
+                   else
+                     "_ml/trained_models"
+                   end
+          params = Utils.process_params(arguments)
+
+          ElasticsearchServerless::API::Response.new(
+            perform_request(method, path, params, body, headers)
+          )
+        end
+      end
+    end
+  end
+end
