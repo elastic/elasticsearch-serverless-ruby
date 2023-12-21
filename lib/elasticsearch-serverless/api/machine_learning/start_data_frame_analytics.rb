0d6f44b51b1f26d38afbb96c7eabc78b8fd6a601
Fernando Briano <fernando@picandocodigo.net>
2023-12-21 11:38:46 +0000

    [API] Adds machine learning data frame analytics endpoints
    * delete_data_frame_analytics.rb
    * evaluate_data_frame.rb
    * get_data_frame_analytics.rb
    * get_data_frame_analytics_stats.rb
    * preview_data_frame_analytics.rb
    * put_data_frame_analytics.rb
    * start_data_frame_analytics.rb
    * stop_data_frame_analytics.rb
    * update_data_frame_analytics.rb

---
+++ lib/elasticsearch-serverless/api/machine_learning/start_data_frame_analytics.rb
@@ -0,0 +1,54 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Auto generated from commit f284cc16f4d4b4289bc679aa1529bb504190fe80
+# @see https://github.com/elastic/elasticsearch-specification
+#
+module ElasticsearchServerless
+  module API
+    module MachineLearning
+      module Actions
+        # Starts a data frame analytics job.
+        #
+        # @option arguments [String] :id Identifier for the data frame analytics job. This identifier can contain lowercase alphanumeric characters (a-z and 0-9), hyphens, and underscores. It must start and end with alphanumeric characters. (*Required*)
+        # @option arguments [Time] :timeout Controls the amount of time to wait until the data frame analytics job starts. Server default: 20s.
+        # @option arguments [Hash] :headers Custom HTTP headers
+        #
+        # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/start-dfanalytics.html
+        #
+        def start_data_frame_analytics(arguments = {})
+          raise ArgumentError, "Required argument 'id' missing" unless arguments[:id]
+
+          arguments = arguments.clone
+          headers = arguments.delete(:headers) || {}
+
+          body = nil
+
+          _id = arguments.delete(:id)
+
+          method = ElasticsearchServerless::API::HTTP_POST
+          path   = "_ml/data_frame/analytics/#{Utils.listify(_id)}/_start"
+          params = Utils.process_params(arguments)
+
+          ElasticsearchServerless::API::Response.new(
+            perform_request(method, path, params, body, headers)
+          )
+        end
+      end
+    end
+  end
+end
