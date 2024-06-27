a078d324ed7ad32e243a601336a592fcc1b3e91b
Fernando Briano <fernando@picandocodigo.net>
2024-06-27 16:36:23 +0100

    [API] Adds machine_learning.update_trained_model_deployment API

---
+++ lib/elasticsearch-serverless/api/machine_learning/update_trained_model_deployment.rb
@@ -0,0 +1,54 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Auto generated from commit f284cc16f4d4b4289bc679aa1529bb504190fe80
+# @see https://github.com/elastic/elasticsearch-specification
+#
+module ElasticsearchServerless
+  module API
+    module MachineLearning
+      module Actions
+        # Starts a trained model deployment, which allocates the model to every machine learning node.
+        # @option arguments [String] :model_id The unique identifier of the trained model. Currently, only PyTorch models are supported. (*Required*)
+        # @option arguments [Integer] :number_of_allocations The number of model allocations on each node where the model is deployed. All allocations on a node share the same copy of the model in memory but use a separate set of threads to evaluate the model. Increasing this value generally increases the throughput. If this setting is greater than the number of hardware threads it will automatically be changed to a value less than the number of hardware threads. Server default: 1.
+        # @option arguments [Hash] :headers Custom HTTP headers
+        # @option arguments [Hash] :body request body
+        #
+        # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/update-trained-model-deployment.html
+        #
+        def update_trained_model_deployment(arguments = {})
+          raise ArgumentError, "Required argument 'model_id' missing" unless arguments[:model_id]
+
+          arguments = arguments.clone
+          headers = arguments.delete(:headers) || {}
+
+          body = arguments.delete(:body)
+
+          _model_id = arguments.delete(:model_id)
+
+          method = ElasticsearchServerless::API::HTTP_POST
+          path   = "_ml/trained_models/#{Utils.listify(_model_id)}/deployment/_update"
+          params = Utils.process_params(arguments)
+
+          ElasticsearchServerless::API::Response.new(
+            perform_request(method, path, params, body, headers)
+          )
+        end
+      end
+    end
+  end
+end
