7561f6ab058e12350fb520b9597fbb50e501c327
Fernando Briano <fernando@picandocodigo.net>
2024-07-04 21:02:38 +0100

    [API] Adds connector endpoints

---
+++ lib/elasticsearch-serverless/api/connector/list.rb
@@ -0,0 +1,58 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Auto generated from commit f284cc16f4d4b4289bc679aa1529bb504190fe80
+# @see https://github.com/elastic/elasticsearch-specification
+#
+module ElasticsearchServerless
+  module API
+    module Connector
+      module Actions
+        # Returns existing connectors.
+        # This functionality is Experimental and may be changed or removed
+        # completely in a future release. Elastic will take a best effort approach
+        # to fix any issues, but experimental features are not subject to the
+        # support SLA of official GA features.
+        #
+        # @option arguments [Integer] :from Starting offset (default: 0)
+        # @option arguments [Integer] :size Specifies a max number of results to get
+        # @option arguments [String, Array] :index_name A comma-separated list of connector index names to fetch connector documents for
+        # @option arguments [String, Array<String>] :connector_name A comma-separated list of connector names to fetch connector documents for
+        # @option arguments [String, Array<String>] :service_type A comma-separated list of connector service types to fetch connector documents for
+        # @option arguments [String] :query A wildcard query string that filters connectors with matching name, description or index name
+        # @option arguments [Hash] :headers Custom HTTP headers
+        #
+        # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/list-connector-api.html
+        #
+        def list(arguments = {})
+          arguments = arguments.clone
+          headers = arguments.delete(:headers) || {}
+
+          body = nil
+
+          method = ElasticsearchServerless::API::HTTP_GET
+          path   = "_connector"
+          params = Utils.process_params(arguments)
+
+          ElasticsearchServerless::API::Response.new(
+            perform_request(method, path, params, body, headers)
+          )
+        end
+      end
+    end
+  end
+end
