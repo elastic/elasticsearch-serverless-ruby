ec1a018ce48fdb373ddd4ba8a02b1dc2fe7fd28a
Fernando Briano <fernando@picandocodigo.net>
2024-10-11 10:31:06 +0100

    [API] Adds security APIs:
    * security.delete_role.rb
    * security.get_builtin_privileges.rb
    * security.get_role.rb
    * security.put_role.rb
    
    Tests are here:
    https://github.com/elastic/elasticsearch-clients-tests/pull/142

---
+++ lib/elasticsearch-serverless/api/security/get_role.rb
@@ -0,0 +1,72 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Auto generated from commit f284cc16f4d4b4289bc679aa1529bb504190fe80
+# @see https://github.com/elastic/elasticsearch-specification
+#
+module ElasticsearchServerless
+  module API
+    module Security
+      module Actions
+        # The role management APIs are generally the preferred way to manage roles, rather than using file-based role management.
+        # The get roles API cannot retrieve roles that are defined in roles files.
+        #
+        # @option arguments [String, Array<String>] :name The name of the role. You can specify multiple roles as a comma-separated list. If you do not specify this parameter, the API returns information about all roles.
+        # @option arguments [Hash] :headers Custom HTTP headers
+        #
+        # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/security-api-get-role.html
+        #
+        def get_role(arguments = {})
+          request_opts = { endpoint: arguments[:endpoint] || "security.get_role" }
+
+          defined_params = [:name].inject({}) do |set_variables, variable|
+            set_variables[variable] = arguments[variable] if arguments.key?(variable)
+            set_variables
+          end
+          request_opts[:defined_params] = defined_params unless defined_params.empty?
+
+          arguments = arguments.clone
+          headers = arguments.delete(:headers) || {}
+
+          body = nil
+
+          _name = arguments.delete(:name)
+
+          method = ElasticsearchServerless::API::HTTP_GET
+          path   = if _name
+                     "_security/role/#{Utils.listify(_name)}"
+                   else
+                     "_security/role"
+                   end
+          params = Utils.process_params(arguments)
+
+          if Array(arguments[:ignore]).include?(404)
+            Utils.rescue_from_not_found {
+              ElasticsearchServerless::API::Response.new(
+                perform_request(method, path, params, body, headers, request_opts)
+              )
+            }
+          else
+            ElasticsearchServerless::API::Response.new(
+              perform_request(method, path, params, body, headers, request_opts)
+            )
+          end
+        end
+      end
+    end
+  end
+end
---
+++ lib/elasticsearch-serverless/api/security/put_role.rb
@@ -0,0 +1,65 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Auto generated from commit f284cc16f4d4b4289bc679aa1529bb504190fe80
+# @see https://github.com/elastic/elasticsearch-specification
+#
+module ElasticsearchServerless
+  module API
+    module Security
+      module Actions
+        # The role management APIs are generally the preferred way to manage roles, rather than using file-based role management.
+        # The create or update roles API cannot update roles that are defined in roles files.
+        #
+        # @option arguments [String] :name The name of the role that is being created or updated. On Elasticsearch Serverless, the role name must begin with a letter or digit and can only contain letters, digits and the characters '_', '-', and '.'. Each role must have a unique name, as this will serve as the identifier for that role. (*Required*)
+        # @option arguments [String] :refresh If +true+ (the default) then refresh the affected shards to make this operation visible to search, if +wait_for+ then wait for a refresh to make this operation visible to search, if +false+ then do nothing with refreshes.
+        # @option arguments [Hash] :headers Custom HTTP headers
+        # @option arguments [Hash] :body request body
+        #
+        # @see https://www.elastic.co/guide/en/elasticsearch/reference/current/security-api-put-role.html
+        #
+        def put_role(arguments = {})
+          request_opts = { endpoint: arguments[:endpoint] || "security.put_role" }
+
+          defined_params = [:name].inject({}) do |set_variables, variable|
+            set_variables[variable] = arguments[variable] if arguments.key?(variable)
+            set_variables
+          end
+          request_opts[:defined_params] = defined_params unless defined_params.empty?
+
+          raise ArgumentError, "Required argument 'body' missing" unless arguments[:body]
+          raise ArgumentError, "Required argument 'name' missing" unless arguments[:name]
+
+          arguments = arguments.clone
+          headers = arguments.delete(:headers) || {}
+
+          body = arguments.delete(:body)
+
+          _name = arguments.delete(:name)
+
+          method = ElasticsearchServerless::API::HTTP_PUT
+          path   = "_security/role/#{Utils.listify(_name)}"
+          params = Utils.process_params(arguments)
+
+          ElasticsearchServerless::API::Response.new(
+            perform_request(method, path, params, body, headers, request_opts)
+          )
+        end
+      end
+    end
+  end
+end
