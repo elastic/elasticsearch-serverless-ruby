e4f245e3d863cc39ab47e5df034a4b1548d3a08e
Fernando Briano <fernando@picandocodigo.net>
2024-10-11 09:05:19 +0100

    [API] Adds machine_learning.infer_trained_model
    Test here:
    https://github.com/elastic/elasticsearch-clients-tests/pull/140

---
+++ lib/elasticsearch-serverless/api/machine_learning/infer_trained_model.rb
@@ -0,0 +1,69 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Auto generated from commit f284cc16f4d4b4289bc679aa1529bb504190fe80
+# @see https://github.com/elastic/elasticsearch-specification
+#
+module ElasticsearchServerless
+  module API
+    module MachineLearning
+      module Actions
+        # Evaluate a trained model.
+        #
+        # @option arguments [String] :model_id The unique identifier of the trained model. (*Required*)
+        # @option arguments [Time] :timeout Controls the amount of time to wait for inference results. Server default: 10s.
+        # @option arguments [Hash] :headers Custom HTTP headers
+        # @option arguments [Hash] :body request body
+        #
+        # *Deprecation notice*:
+        # /_ml/trained_models/{model_id}/deployment/_infer is deprecated. Use /_ml/trained_models/{model_id}/_infer instead
+        # Deprecated since version 8.3.0
+        #
+        #
+        # @see https://www.elastic.co/guide/en/elasticsearch/reference/master/infer-trained-model.html
+        #
+        def infer_trained_model(arguments = {})
+          request_opts = { endpoint: arguments[:endpoint] || "ml.infer_trained_model" }
+
+          defined_params = [:model_id].inject({}) do |set_variables, variable|
+            set_variables[variable] = arguments[variable] if arguments.key?(variable)
+            set_variables
+          end
+          request_opts[:defined_params] = defined_params unless defined_params.empty?
+
+          raise ArgumentError, "Required argument 'body' missing" unless arguments[:body]
+          raise ArgumentError, "Required argument 'model_id' missing" unless arguments[:model_id]
+
+          arguments = arguments.clone
+          headers = arguments.delete(:headers) || {}
+
+          body = arguments.delete(:body)
+
+          _model_id = arguments.delete(:model_id)
+
+          method = ElasticsearchServerless::API::HTTP_POST
+          path   = "_ml/trained_models/#{Utils.listify(_model_id)}/_infer"
+          params = Utils.process_params(arguments)
+
+          ElasticsearchServerless::API::Response.new(
+            perform_request(method, path, params, body, headers, request_opts)
+          )
+        end
+      end
+    end
+  end
+end
