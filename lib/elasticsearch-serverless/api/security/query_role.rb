3b03baa3eb4c802e45cb97f7ee9c393644a116c6
Fernando Briano <fernando@picandocodigo.net>
2025-02-21 12:31:17 +0000

    [API] Adds security.query_role endpoint

---
+++ lib/elasticsearch-serverless/api/security/query_role.rb
@@ -0,0 +1,61 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Auto generated from commit f284cc16f4d4b4289bc679aa1529bb504190fe80
+# @see https://github.com/elastic/elasticsearch-specification
+#
+module ElasticsearchServerless
+  module API
+    module Security
+      module Actions
+        # Find roles with a query.
+        # Get roles in a paginated manner.
+        # The role management APIs are generally the preferred way to manage roles, rather than using file-based role management.
+        # The query roles API does not retrieve roles that are defined in roles files, nor built-in ones.
+        # You can optionally filter the results with a query.
+        # Also, the results can be paginated and sorted.
+        #
+        # @option arguments [Hash] :headers Custom HTTP headers
+        # @option arguments [Hash] :body request body
+        #
+        # @see https://www.elastic.co/docs/api/doc/elasticsearch/operation/operation-security-query-role
+        #
+        def query_role(arguments = {})
+          request_opts = { endpoint: arguments[:endpoint] || 'security.query_role' }
+
+          arguments = arguments.clone
+          headers = arguments.delete(:headers) || {}
+
+          body = arguments.delete(:body)
+
+          method = if body
+                     ElasticsearchServerless::API::HTTP_POST
+                   else
+                     ElasticsearchServerless::API::HTTP_GET
+                   end
+
+          path   = "_security/_query/role"
+          params = {}
+
+          ElasticsearchServerless::API::Response.new(
+            perform_request(method, path, params, body, headers, request_opts)
+          )
+        end
+      end
+    end
+  end
+end
